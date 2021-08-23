1bc44bb029eb942d45dc75b7666da85423404a90
dani <dani.zaghian@gmail.com>
2021-08-23 11:07:57 -0700

    adds dynamic time and multiple routes

--- app/controllers/example_pages_controller.rb
+++ app/controllers/example_pages_controller.rb
@@ -2,7 +2,14 @@ class ExamplePagesController < ApplicationController
 
   def hello_method
     # web response
-    render json: {message: "Hello World!"}
+    render json: {
+      message: "Hello World!", 
+      time: Time.now.strftime("%b %e, %l:%M %p")
+    }
+  end
+
+  def goodbye_method
+    render json: {message: "Farewell!"}
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get "/hello_path", controller: "example_pages", action: "hello_method"
+  get "/goodbye_path", controller: "example_pages", action: "goodbye_method"
 end
